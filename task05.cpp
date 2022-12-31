0bee07f7d7d07103a8f99d154166e8a94cdb4438
d4same <120159334+d4same@users.noreply.github.com>
2022-12-31 14:23:21 +0500

    upload

--- task05.cpp
+++ task05.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 main()
 {
-cout<<"***********************************************************"<<endl;
-cout<<"*                                                         *"<<endl;
-cout<<"*                                                         *"<<endl;
-cout<<"*      University Admission                               *"<<endl;
-cout<<"*       Management System                                 *"<<endl;
-cout<<"*                                                         *"<<endl;
-cout<<"*                                                         *"<<endl;
-cout<<"***********************************************************"<<endl;
+ int number1;
+ int number2;
+ cout<<"enter a number1"<<endl;
+ cin>>number1;
+ cout<<"enter a number2"<<endl;
+ cin>>number2;
+ if(number1>number2)
+ {
+ cout<<"number 1 is larger than number2"<<number1<<endl;
+ }
+ if(number1<number2)
+ {
+ cout<<"number2 is greater than number 1 "<<number2<<endl;
+ }
 }
